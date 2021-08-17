8a6f4a88422a7a7eddd05ef3ea78036f203d113e
caio <caiobr98@gmail.com>
2021-08-17 15:18:38 -0300

    ranquei a virgula

--- resources/views/layouts/appfora.blade.php
+++ resources/views/layouts/appfora.blade.php
@@ -8,7 +8,7 @@
     <link href="images/" rel="icon">
     <link rel="stylesheet" href="{{ asset('css/plugins.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/green.css') }}">,
+    <link rel="stylesheet" href="{{ asset('css/green.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
