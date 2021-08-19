dc2ded031865bae78f1eef627473c7046eda103e
caio <caiobr98@gmail.com>
2021-08-19 10:18:10 -0300

    string

--- config/constants.php
+++ config/constants.php
@@ -6,6 +6,7 @@ $uniqueNumber = rand(1000000000, 9999999999);
 
 $galaxId = env('APP_ENV') === 'production' ? "16435" : "5473" ;
 $galaxHash = env('APP_ENV') === 'production' ? "OfGg7iE05bWlWeO0GoW1W96xI4L10i28DeAcX0Mw" : "83Mw5u8988Qj6fZqS4Z8K7LzOo1j28S706R0BeFe" ;
+$galaxUrl = env('APP_ENV') === 'production' ? "https://api.galaxpay.com.br/v2/" : "https://api.sandbox.cloud.galaxpay.com.br/v2/" ;
 
 return [
     'pay_request' => 'pay-request-' . date("d-m-Y") . '-' . $uniqueNumber,
@@ -13,7 +14,7 @@ return [
     'pay_creditcard' => 'pay-custom-creditcard-' . date("d-m-Y") . '-' . $uniqueNumber,
     'galaxId' => (string) $galaxId,
     'galaxHash' => (string) $galaxHash,
-    'galaxUrl' => (string)  env('APP_ENV') === 'production' ? "https://api.galaxpay.com.br/v2/" : "https://api.sandbox.cloud.galaxpay.com.br/v2/",
+    'galaxUrl' => (string) $galaxUrl,
     'galaxPlanoBronze' => (string) "pay-602526be3d5cfkass.43389561",
     'galaxPlanoPrata' => (string) "pay-602526be3d5cfkass.43389562",
     'galaxPlanoBronzePlus' => (string) "pay-602526be3d5cfkass.43389564",
