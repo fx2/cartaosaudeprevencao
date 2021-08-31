e91730d428bb5c56f30b182add7fd8de70f980ee
Caio do Amaral Alves <caio.alves@superteia.com.br>
2021-08-31 19:28:44 -0300

    s

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+header('Access-Control-Allow-Origin: *'); header('Access-Control-Allow-Methods: *'); header('Access-Control-Allow-Headers: Origin, X-Requested-With,Authorization, Content-Type, Accept');
+
 define('LARAVEL_START', microtime(true));
 
 /*
