6d74d83c032f65b4b97a7bfde6f745e33cab024d
Caio do Amaral Alves <caio.alves@superteia.com.br>
2021-08-31 17:36:27 -0300

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class Cors
 {
@@ -13,16 +14,8 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-XSRF-TOKEN');
-        return $response;
-        // return $next($request)
-        //     ->header('Access-Control-Allow-Origin', "*")
-        //     ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
-        //     ->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type");
+        return $next($request);
     }
 }
