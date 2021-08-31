670f11f7ec83fb4969cb93644ee39aaebdf49bf0
Caio do Amaral Alves <caio.alves@superteia.com.br>
2021-08-31 19:39:51 -0300

    a

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@
 */
 
 header('Access-Control-Allow-Origin', '*');
-header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS');
+header('Access-Control-Allow-Methods', '*');
 header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
 $app = new Illuminate\Foundation\Application(
