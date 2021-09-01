aa6f3e5119ad38f38831b2bf51f9209ddb2c4170
Caio do Amaral Alves <caio.alves@superteia.com.br>
2021-09-01 00:14:03 -0300

    a

--- resources/views/layouts/appfora.blade.php
+++ resources/views/layouts/appfora.blade.php
@@ -21,7 +21,7 @@
 
     <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
-    {{-- <link rel="stylesheet" href="{{ asset('css/loading.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/loading.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
