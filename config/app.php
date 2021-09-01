52650358e74429a8632d9080ec980659200df9ae
Caio do Amaral Alves <caio.alves@superteia.com.br>
2021-08-31 21:34:12 -0300

    teste

--- config/app.php
+++ config/app.php
@@ -1,9 +1,5 @@
 <?php
 return [
-    header('Access-Control-Allow-Origin: *'),
-    header('Access-Control-Allow-Methods: *'), 
-    header('Access-Control-Allow-Headers: Origin, X-Requested-With,Authorization, Content-Type, Accept'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -163,6 +159,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Package Service Providers...
