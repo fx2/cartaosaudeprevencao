5b04218e78d666c41eb3558819605a294d120ae5
Caio do Amaral Alves <caio.alves@superteia.com.br>
2021-09-01 09:31:24 -0300

    c

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,10 +59,10 @@
         {{-- Login field --}}
         <div class="row">
             <div class="col-7">
-                <div class="icheck-primary">
+                {{-- <div class="icheck-primary">
                     <input type="checkbox" name="remember" id="remember">
                     <label for="remember">{{ __('adminlte::adminlte.remember_me') }}</label>
-                </div>
+                </div> --}}
             </div>
             <div class="col-5">
                 <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
@@ -77,13 +77,13 @@
 
 @section('auth_footer')
     {{-- Password reset link --}}
-    @if($password_reset_url)
+    {{-- @if($password_reset_url)
         <p class="my-0">
             <a href="{{ $password_reset_url }}">
                 {{ __('adminlte::adminlte.i_forgot_my_password') }}
             </a>
         </p>
-    @endif
+    @endif --}}
 
     {{-- Register link --}}
     {{-- @if($register_url)
