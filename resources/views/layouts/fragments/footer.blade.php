68a67faae34cdca0be10aef8d2982e66515d5a31
Caio do Amaral Alves <caio.alves@superteia.com.br>
2021-09-16 20:31:45 -0300

    correcao marota

--- resources/views/layouts/fragments/footer.blade.php
+++ resources/views/layouts/fragments/footer.blade.php
@@ -8,11 +8,8 @@
                         <h2 class="footer-heading">Cartão Saúde Prevenção</h2>
                         <p>Reunimos em um único Cartão de Desconto, saúde, educação, entretenimento, entre outros diversos segmentos. Buscamos a disponibilidade de descontos e benefícios em todas às suas necessidades.
 .</p>
-                        <ul class="ftco-footer-social p-0">
-                <li class="ftco-animate"><a href="https://www.facebook.com/cartaosaudeprevencao" target="blank" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="fa fa-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="https://instagram.com/cartaosaudeprevencaoo?igshid=aq6svdz5mvov" target="blank" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="fa fa-instagram"></span></a></li>
-            </ul>
                     </div>
+                    
                     <div class="col-md-8">
                         <div class="row justify-content-center">
                             <div class="col-md-12 col-lg-10">
@@ -20,35 +17,67 @@
                                     <div class="col-md-4 mb-md-0 mb-4">
                                         <h2 class="footer-heading">Planos</h2>
                                         <ul class="list-unstyled">
-                              <li><a href="#beneficio" class="py-1 d-block">Benefícios</a></li>
-                              <li><a href="#atendimento" class="py-1 d-block">Atendimento </a></li>
-                              <li><a href="#exames" class="py-1 d-block">Exames</a></li>
-                              <li><a href="#laboratorios" class="py-1 d-block">Laboratorios</a></li>
-                            </ul>
+                                        <li><a href="#beneficio" class="py-1 d-block">Benefícios</a></li>
+                                        <li><a href="#atendimento" class="py-1 d-block">Atendimento </a></li>
+                                        <li><a href="#exames" class="py-1 d-block">Exames</a></li>
+                                        <li><a href="#laboratorios" class="py-1 d-block">Laboratorios</a></li>
+                                        </ul>
                                     </div>
                                     <div class="col-md-4 mb-md-0 mb-4">
                                         <h2 class="footer-heading">Descontos</h2>
                                         <ul class="list-unstyled">
-                              <li><a href="#saude" class="py-1 d-block">Saúde</a></li>
-                              <li><a href="#saude" class="py-1 d-block">Compras</a></li>
-                              <li><a href="#saude" class="py-1 d-block">Entretenimento </a></li>
-                              <li><a href="#saude" class="py-1 d-block">Filmes</a></li>
-                            </ul>
+                                            <li><a href="#saude" class="py-1 d-block">Saúde</a></li>
+                                            <li><a href="#saude" class="py-1 d-block">Compras</a></li>
+                                            <li><a href="#saude" class="py-1 d-block">Entretenimento </a></li>
+                                            <li><a href="#saude" class="py-1 d-block">Filmes</a></li>
+                                        </ul>
                                     </div>
                                     <div class="col-md-4 mb-md-0 mb-4">
                                         <h2 class="footer-heading">Atendimento</h2>
                                         <ul class="list-unstyled">
-                              <li><a href="#rede" class="py-1 d-block">Rede de Parceiros</a></li>
-                              <li><a href="#rede" class="py-1 d-block">24 horas</a></li>
-                              <li><a href="#rede" class="py-1 d-block">Grande São Paulo</a></li>
-                              <li><a href="#rede" class="py-1 d-block">Interior</a></li>
-                            </ul>
+                                        <li><a href="#rede" class="py-1 d-block">Rede de Parceiros</a></li>
+                                        <li><a href="#rede" class="py-1 d-block">24 horas</a></li>
+                                        <li><a href="#rede" class="py-1 d-block">Grande São Paulo</a></li>
+                                        <li><a href="#rede" class="py-1 d-block">Interior</a></li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-md-4 mb-md-0 mb-4">
+                        <h2 class="footer-heading">Endereço</h2>
+                        <p>Rua João Vagnotti, Nº 352. Centro, Itaquaquecetuba/SP – Atrás das Casas Bahia </p>
+                        <!-- <a class="pickus" href="#." data-text="Get Directions"></a> -->
+                    </div>
+                    <div class="col-md-4 mb-md-0 mb-4">
+                        <h2 class="footer-heading">Telefones</h2>
+                        <p>Agendamento:
+                        <p> (11) 4730-0576 / (11) 93505-4410</p> <span class="block">
+                            <p>Whatsapp: (11) 93505-4410
+                        </span> </p>
+                        <!-- <a class="pickus" href="#." data-text="Call Us">Fale agora</a> -->
+                    </div>
+                    <div class="col-md-4 mb-md-0 mb-4">
+                        <h2 class="footer-heading">E-mail</h2>
+                        <p>Agendamento: contato@cartaosaudeprevencao.com.br
+                        <p> <span class="block">Sac: contato@cartaoprevencaosaude.com.br</span> </p>
+                        <!-- <a class="pickus" href="#." data-text="Send a Message">Envie</a> -->
+                    </div>
+                    <div class="col-md-4 mb-md-0 mb-4">
+                        <h2 class="footer-heading">Meu Cartão</h2>
+                        <p>Cartão: cartao@cartãosaudeprevencao.com.br
+                        <p> <span>Financeiro: financeiro@cartaosaudeprevencao.com.br </span> </p>
+                        <!-- <a class="pickus" href="#." data-text="Open a Ticket">Fale Agora</a> -->
+                        <ul class="ftco-footer-social p-0">
+                            <li class="ftco-animate"><a href="https://www.facebook.com/cartaosaudeprevencao" target="blank" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="fa fa-facebook"></span></a></li>
+                            <li class="ftco-animate"><a href="https://instagram.com/cartaosaudeprevencaoo?igshid=aq6svdz5mvov" target="blank" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="fa fa-instagram"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                
                 <div class="row mt-md-5">
                     <div class="col-md-12">
                         <p class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
